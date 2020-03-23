24d4498ad7df30629779dc0c0369b050feebe245
denys <dexixoxa@yahoo.com>
2020-03-23 12:12:49 +0100

    Initial Commit

---
+++ platform 2D movement with dravityTests/platform_2D_movement_with_dravityTests.swift
@@ -0,0 +1,34 @@
+//
+//  platform_2D_movement_with_dravityTests.swift
+//  platform 2D movement with dravityTests
+//
+//  Created by admin on 23/03/2020.
+//  Copyright © 2020 DenysPashkov. All rights reserved.
+//
+
+import XCTest
+@testable import platform_2D_movement_with_dravity
+
+class platform_2D_movement_with_dravityTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
