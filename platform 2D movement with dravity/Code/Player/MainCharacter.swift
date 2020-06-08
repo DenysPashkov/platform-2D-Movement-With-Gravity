a78c56af9f2fd3fb9843127b01af43a6ee888f1e
dextronder97 <dexixoxa@yahoo.it>
2020-06-08 20:55:43 +0200

    .

--- platform 2D movement with dravity/Code/Player/MainCharacter.swift
+++ platform 2D movement with dravity/Code/Player/MainCharacter.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+import GameplayKit
+import SpriteKit
+
+class MainCharacter{
+	
+	var player : SKSpriteNode
+	
+//	MARK: Player Characteristic
+	
+	private let playerSpeed = CGFloat(0.12)
+	private let playerColor = UIColor.orange
+	
+	private var isFalling : Bool
+	private var isDJReady : Bool
+	var isDashCD : Bool
+	
+//	MARK: Player Init
+	
+	init(cNode : SKSpriteNode) {
+		
+		player = cNode
+		player.scene?.scaleMode = .aspectFill
+		player.color = playerColor
+		isFalling = false
+		isDJReady = false
+		isDashCD = false
+		
+	}
+	
+//	MARK: Movement System
+	
+	func xMove(direction : CGFloat) {
+		player.position.x += playerSpeed * direction
+	}
+	
+	func jump(){
+		if !isFalling {
+			isFalling = true
+			player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 120))
+		}
+	}
+	
+	func doubleJump() {
+		if isDJReady && isFalling{
+			isDJReady = false
+			player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
+		}
+	}
+	
+	func dash(direction : CGFloat) -> Bool {
+
+		if direction == 0 || isDashCD { return false } else {
+			isDashCD = true
+			
+			player.position.x += 100 * ( direction > 0 ? 1 : -1 )
+			
+			DispatchQueue.main.asyncAfter(deadline: .now() + 1 ) { self.isDashCD = false }
+			return true
+		}
+	}
+	
+//	MARK: Utility Function
+	
+	func groundTouched(){
+		isFalling = false
+		isDJReady = true
+	}
+	
+}
--- platform 2D movement with dravity/Code/Views/GameScene.swift
+++ platform 2D movement with dravity/Code/Views/GameScene.swift
@@ -9,102 +9,159 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+	
+    private var initialTouch: CGPoint = CGPoint.zero
+	private var endTouch : CGPoint = CGPoint.zero
+	private var touchStarted: TimeInterval?
+	
+	var oldCurrentTime : Double = 0
+	var character : MainCharacter!
+	
+	private let backgroundCustomColor = UIColor.green
+	
+	private let moveJoystick = TLAnalogJoystick(withDiameter: 100)
+	
+	override func didMove(to view: SKView) {
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
+		physicsWorld.contactDelegate = self
+		
+		self.backgroundColor =  backgroundCustomColor
+		
+		spawnPlayer()
+		manageJoystick()
+		gestureSetting()
+		
+		view.isMultipleTouchEnabled = true
+	}
+	
+//	MARK: Recognize Player
+	
+	func spawnPlayer() {
+		
+		if let characterNode = childNode(withName: "MainCharacter") as? SKSpriteNode{
+			character = MainCharacter(cNode: characterNode)
+			character.player.texture?.filteringMode = .nearest
+		}
+		
+	}
+		
+//	MARK: Setting Movement
+	
+	func manageJoystick(){
+		
+		var image = UIImage(named: "jStick")
+		moveJoystick.handleImage = image
+		image = UIImage(named: "jSubstrate")
+		moveJoystick.baseImage = image
+		
+		let moveJoystickHiddenArea = TLAnalogJoystickHiddenArea(rect: CGRect(x: -frame.width / 2, y: frame.height / 2, width: frame.width/2, height: -frame.height))
+		moveJoystickHiddenArea.strokeColor = UIColor.black.withAlphaComponent(0)
+		moveJoystickHiddenArea.joystick = moveJoystick
+		moveJoystick.isMoveable = true
+		
+		camera!.addChild(moveJoystickHiddenArea)
+		
+		moveJoystick.on(.move) { [unowned self] joystick in
+
+			self.character.xMove(direction: joystick.velocity.x)
+
+		}
+		
+		moveJoystick.on(.end) { (joystick) in
+			<#code#>
+		}
+	}
+//	start Touch
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if let touch = touches.first{
+			initialTouch = touch.location(in: self.scene!)
+			endTouch = initialTouch
+		}
+	}
+//	Moving
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		 if let touch = touches.first?.location(in: self.scene!){ endTouch = touch }
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		
+		if endTouch.x - initialTouch.x > 150 || endTouch.x - initialTouch.x < -150 {
+			print(endTouch.x - initialTouch.x > 150 ? " X positive" : " X negative")
+		} else if endTouch.y - initialTouch.y > 150 || endTouch.y - initialTouch.y < -150 {
+			print(endTouch.y - initialTouch.y > 150 ? " Y positive" : " Y negative")
+		}
+	}
+	
+//	MARK: Gesture Implementation
+	
+	func gestureSetting(){
+		
+			let shortTapGesture = UITapGestureRecognizer(target: camera?.scene, action: #selector(shortTap(_:)))
+			camera?.scene?.view?.addGestureRecognizer(shortTapGesture)
+			
+			let longTapGesture = UILongPressGestureRecognizer(target: camera?.scene, action: #selector(longPress(_:)))
+			longTapGesture.minimumPressDuration = 1
+			camera?.scene?.view?.addGestureRecognizer(longTapGesture)
+			
+		}
+		
+	@objc func shortTap(_ sender: UITapGestureRecognizer) {
+		if (sender.location(in: camera?.scene?.view).x) > (camera?.scene?.view?.frame.size.width)! / 2 {
+			character.doubleJump()
+			character.jump()
+		}
+	}
+	
+	@objc func longPress(_ sender: UILongPressGestureRecognizer){
+		
+		if (sender.location(in: camera?.scene?.view).x) > (camera?.scene?.view?.frame.size.width)! / 2 {
+			if character.dash(direction: moveJoystick.velocity.x){
+				shakeCamera(layer: camera!, duration: 0.2)
+			}
+		}
+	}
+	
+//	MARK: Physic contact
+	
+	func didBegin(_ contact: SKPhysicsContact) {
+		
+		var firstBody : SKPhysicsBody
+		var secondBody : SKPhysicsBody
+		
+		if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+			firstBody = contact.bodyA
+			secondBody = contact.bodyB
+		} else {
+			secondBody = contact.bodyA
+			firstBody = contact.bodyB
+		}
+		
+		if firstBody.categoryBitMask == UInt32(1) && secondBody.categoryBitMask == UInt32(3){
+			character.groundTouched()
+		}
+		
+	}
+	
+//	MARK: Utility function
+	
+	func shakeCamera(layer:SKCameraNode, duration:Float) {
+
+		let amplitudeX:Float = 10;
+		let amplitudeY:Float = 6;
+		let numberOfShakes = duration / 0.04;
+		var actionsArray:[SKAction] = [];
+		for _ in 1...Int(numberOfShakes) {
+			let moveX = Float(arc4random_uniform(UInt32(amplitudeX))) - amplitudeX / 2;
+			let moveY = Float(arc4random_uniform(UInt32(amplitudeY))) - amplitudeY / 2;
+			let shakeAction = SKAction.moveBy(x: CGFloat(moveX), y: CGFloat(moveY), duration: 0.02);
+			shakeAction.timingMode = SKActionTimingMode.easeOut;
+			actionsArray.append(shakeAction);
+			actionsArray.append(shakeAction.reversed());
+		}
+
+		let actionSeq = SKAction.sequence(actionsArray);
+		layer.run(actionSeq);
+	}
+	
 }
